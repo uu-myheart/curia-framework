335ec5a4706c0f441cd123ae7ea3558c48a264f1
baigali <baigalikkk@163.com>
2018-06-23 00:20:32 +0800

    Method binding

--- Application.php
+++ Application.php
@@ -4,7 +4,28 @@ namespace Curia\Framework;
 
 use Curia\Container\Container;
 
-class Application extends Contaner
+class Application extends Container
 {
+    protected $bathPath;
 
+    public function __construct($bathPath = null)
+    {
+        $this->bathPath = $bathPath;
+
+        static::setInstance($this);
+    }
+
+    public function register($provider)
+    {
+        if (method_exists($provider, 'register')) {
+            $provider->register();
+        }
+
+        //TODO $provider->boot();
+    }
+
+    public function bathPath()
+    {
+        return $this->bathPath;
+    }
 }
